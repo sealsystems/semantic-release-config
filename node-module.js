fff6a8419ae21e9e822bed77d39d72bc44973e13
Michael Scherer <michael.scherer@sealsystems.de>
2018-09-08 21:40:45 +0200

    Fix index error

--- node-module.js
+++ node-module.js
@@ -2,12 +2,12 @@
 
 const result = require('./generic');
 
-result.verifyConditions.splice(1, 0, '@semantic-release/npm');
-result.prepare.splice(1, 0, '@semantic-release/npm');
-
 // Releases of modules may be ignored by the CI
 /* eslint-disable no-template-curly-in-string */
 result.prepare[1].message = '[ci skip] ${result.prepare[1].message}';
 /* eslint-enable no-template-curly-in-string */
+
+result.verifyConditions.splice(1, 0, '@semantic-release/npm');
+result.prepare.splice(1, 0, '@semantic-release/npm');
 
 module.exports = result;
