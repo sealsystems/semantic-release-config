7918417a63dc0a9eccd6ed6805b997571c0f6470
Michael Scherer <michael.scherer@sealsystems.de>
2018-09-08 22:58:29 +0200

    Do not add 'ci skip' to release commits since CicleCI badge gets confused otherwise

--- node-module.js
+++ node-module.js
@@ -5,10 +5,10 @@ const cloneDeep = require('clone-deep');
 const nodeBinary = require('./node-binary');
 const result = cloneDeep(nodeBinary);
 
-// Releases of modules may be ignored by the CI
-/* eslint-disable no-template-curly-in-string */
-result.prepare[2].message = `[ci skip] ${result.prepare[2].message}`;
-/* eslint-enable no-template-curly-in-string */
+// // Releases of modules may be ignored by the CI
+// /* eslint-disable no-template-curly-in-string */
+// result.prepare[2].message = `[ci skip] ${result.prepare[2].message}`;
+// /* eslint-enable no-template-curly-in-string */
 result.npmPublish = true;
 
 module.exports = result;
